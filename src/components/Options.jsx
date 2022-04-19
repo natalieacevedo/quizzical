348c092172e99f293dbc73d0ead02e22adf2eab2
Natalie Acevedo <natalieacevedo25@gmail.com>
2022-04-19 22:22:10 +0100

    able to get id of category and difficulty

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -3,84 +3,66 @@ import React, { useEffect, useState } from "react";
 import Questions from "./Questions";
 
 function Options() {
+  const [userOptions, setUserOptions] = useState({
+    category: "https://opentdb.com/api.php?amount=10",
+    difficulty: "",
+  });
+
+  function getUserInput(event) {
+    const { value, id } = event.target;
+    //console.log(value, id);
+
+    setUserOptions((prevData) => {
+      let difficultyLevel = id === "difficulty" ? value : "";
+      let categorySelected = id === "category" ? value : "";
+
+      return {
+        ...prevData,
+        category:
+          categorySelected === "" ? prevData["category"] : categorySelected,
+        difficulty:
+          difficultyLevel === "" ? prevData["difficulty"] : difficultyLevel,
+      };
+    });
+  }
+  console.log(userOptions);
+
   return (
-    <form className="allQuestions">
-      <fieldset>
-        <legend>Select a category</legend>
-        <input type="radio" id="Any Category" />
-        <label htmlFor="Any Category">Any Category</label>
-        <br />
-        <input type="radio" id="General Knowledge" />
-        <label htmlFor="General Knowledge">General Knowledge</label>
-        <br />
-        <input type="radio" id="Celebrities" />
-        <label htmlFor="Celebrities">Celebrities</label>
-        <br />
+    <section onChange={getUserInput}>
+      <label htmlFor="Category">Select a Category</label>
+      <select name="Category" id="category">
+        <option value="">Please choose an option</option>
+        <option value="Any Category">Any Category</option>
+        <option value="9">General Knowledge</option>
+        <option value="26">Celebrities</option>
+        <option value="10">Books</option>
+        <option value="25">Art</option>
+        <option value="23">History</option>
+        <option value="18">Computers</option>
+        <option value="20">Mythology</option>
+        <option value="11">Film</option>
+        <option value="12">Music</option>
+        <option value="15">Video Games</option>
+        <option value="17">Science and Nature</option>
+        <option value="31"> Anime and Manga </option>
+        <option value="27">Animals</option>
+        <option value="19">Mathematics</option>
+        <option value="28"> Vehicles</option>
+        <option value="29">Comics</option>
+        <option value="30"> Gadgets</option>
+      </select>
 
-        <input type="radio" id="Books" />
-        <label htmlFor="Books">Books</label>
-
-        <br />
-        <input type="radio" id="Art" />
-        <label htmlFor="Art">Art</label>
-        <br />
-        <input type="radio" id="History" />
-        <label htmlFor="History">History</label>
-        <br />
-        <input type="radio" id="Computers" />
-        <label htmlFor="Computers">Computers</label>
-        <br />
-        <input type="radio" id="Mythology" />
-        <label htmlFor="Mythology">Mythology</label>
-        <br />
-
-        <input type="radio" id="Film" />
-        <label htmlFor="Film">Film</label>
-        <br />
-        <input type="radio" id="Music" />
-        <label htmlFor="Music">Music</label>
-        <br />
-        <input type="radio" id="Video Games" />
-        <label htmlFor="Video Games">Video Games</label>
-        <br />
-        <input type="radio" id="Science and Nature" />
-        <label htmlFor="Science and Nature">Science and Nature</label>
-        <br />
-        <input type="radio" id="Anime and Manga" />
-        <label htmlFor="Anime and Manga">Anime and Manga</label>
-        <br />
-        <input type="radio" id="Animals" />
-        <label htmlFor="Animals">Animals</label>
-        <br />
-        <input type="radio" id="Mathematics" />
-        <label htmlFor="Mathematics">Mathematics</label>
-        <br />
-        <input type="radio" id="Vehicles" />
-        <label htmlFor="Vehicles">Vehicles</label>
-        <br />
-        <input type="radio" id="Comics" />
-        <label htmlFor="Comics">Comics</label>
-        <br />
-        <input type="radio" id="Gadgets" />
-        <label htmlFor="Gadgets">Gadgets</label>
-        <br />
-      </fieldset>
-      <fieldset>
-        <legend>Select difficulty</legend>
-        <input type="radio" id="Any difficulty" />
-        <label htmlFor="Any difficulty">Any difficulty</label>
-        <br />
-        <input type="radio" id="Easy" />
-        <label htmlFor="Easy">Easy</label>
-        <br />
-        <input type="radio" id="Medium" />
-        <label htmlFor="Medium">Medium</label>
-        <br />
-        <input type="radio" id="Hard" />
-        <label htmlFor="Hard">Hard</label>
-        <br />
-      </fieldset>
-    </form>
+      <label htmlFor="difficulty">Select Difficulty</label>
+      <select name="difficulty" id="difficulty">
+        <option value="">Choose Difficulty</option>
+        <option value="Any Difficulty">Any Difficulty</option>
+        <option name="easy" value="easy">
+          Easy
+        </option>
+        <option value="medium">Medium</option>
+        <option value="hard">Hard</option>
+      </select>
+    </section>
   );
 }
 
