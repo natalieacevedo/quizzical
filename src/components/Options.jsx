d6535131a9bbc7525405381abfd392354295b4bb
Natalie Acevedo <natalieacevedo25@gmail.com>
2022-04-20 11:07:02 +0100

    conditional call to api was succesful

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,4 +1,3 @@
-//import axios from "axios";
 import React, { useState } from "react";
 import Questions from "./Questions";
 
@@ -33,47 +32,50 @@ function Options() {
   }
   console.log(userOptions);
 
-  return (
-    <section onChange={getUserInput}>
-      <label htmlFor="Category">Select a Category</label>
-      <select name="Category" id="category">
-        <option value="">Please choose an option</option>
-        <option value="Any Category">Any Category</option>
-        <option value="9">General Knowledge</option>
-        <option value="26">Celebrities</option>
-        <option value="10">Books</option>
-        <option value="25">Art</option>
-        <option value="23">History</option>
-        <option value="18">Computers</option>
-        <option value="20">Mythology</option>
-        <option value="11">Film</option>
-        <option value="12">Music</option>
-        <option value="15">Video Games</option>
-        <option value="17">Science and Nature</option>
-        <option value="31"> Anime and Manga </option>
-        <option value="27">Animals</option>
-        <option value="19">Mathematics</option>
-        <option value="28"> Vehicles</option>
-        <option value="29">Comics</option>
-        <option value="30"> Gadgets</option>
-      </select>
+  if (!getQuestions) {
+    return (
+      <section onChange={getUserInput}>
+        <label htmlFor="Category">Select a Category</label>
+        <select name="Category" id="category">
+          <option value="">Please choose an option</option>
+          <option value="Any Category">Any Category</option>
+          <option value="9">General Knowledge</option>
+          <option value="26">Celebrities</option>
+          <option value="10">Books</option>
+          <option value="25">Art</option>
+          <option value="23">History</option>
+          <option value="18">Computers</option>
+          <option value="20">Mythology</option>
+          <option value="11">Film</option>
+          <option value="12">Music</option>
+          <option value="15">Video Games</option>
+          <option value="17">Science and Nature</option>
+          <option value="31"> Anime and Manga </option>
+          <option value="27">Animals</option>
+          <option value="19">Mathematics</option>
+          <option value="28"> Vehicles</option>
+          <option value="29">Comics</option>
+          <option value="30"> Gadgets</option>
+        </select>
 
-      <label htmlFor="difficulty">Select Difficulty</label>
-      <select name="difficulty" id="difficulty">
-        <option value="">Choose Difficulty</option>
-        <option value="Any Difficulty">Any Difficulty</option>
-        <option name="easy" value="easy">
-          Easy
-        </option>
-        <option value="medium">Medium</option>
-        <option value="hard">Hard</option>
-      </select>
-      <button onClick={() => setGetQuestions(!getQuestions)}>
-        See questions
-      </button>
-      {getQuestions && <Questions />}
-    </section>
-  );
+        <label htmlFor="difficulty">Select Difficulty</label>
+        <select name="difficulty" id="difficulty">
+          <option value="">Choose Difficulty</option>
+          <option value="Any Difficulty">Any Difficulty</option>
+          <option name="easy" value="easy">
+            Easy
+          </option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+        </select>
+        <button onClick={() => setGetQuestions(!getQuestions)}>
+          See questions
+        </button>
+      </section>
+    );
+  } else {
+    return <Questions options={userOptions} />;
+  }
 }
 
 export default Options;
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,14 +1,55 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function Questions() {
-  // function getData() {
-  //   axios
-  //     .get("https://opentdb.com/api.php?amount=10")
-  //     .then((response) => response.data)
-  //     .then((data) => setAllQuestions(data.results));
-  // }
+function Questions({ options }) {
+  const [questions, setAllQuestions] = useState(null);
 
-  // useEffect(getData, []);
+  console.log(options);
+
+  function getData() {
+    if (
+      options["category"] === "Any Category" &&
+      options["difficulty"] === "Any Difficulty"
+    ) {
+      axios
+        .get("https://opentdb.com/api.php?amount=10")
+        .then((response) => response.data)
+        .then((data) => setAllQuestions(data.results));
+    } else if (
+      options["category"] === "Any Category" &&
+      options["difficulty"] !== "Any Difficulty"
+    ) {
+      axios
+        .get(
+          `https://opentdb.com/api.php?amount=10&difficulty=${options["difficulty"]}`
+        )
+        .then((response) => response.data)
+        .then((data) => setAllQuestions(data.results));
+    } else if (
+      options["category"] !== "Any Category" &&
+      options["difficulty"] === "Any Difficulty"
+    ) {
+      axios
+        .get(
+          `https://opentdb.com/api.php?amount=10&category=${options["category"]}`
+        )
+        .then((response) => response.data)
+        .then((data) => setAllQuestions(data.results));
+    } else {
+      axios
+        .get(
+          `https://opentdb.com/api.php?amount=10&category=${options["category"]}&difficulty=${options["difficulty"]}`
+        )
+        .then((response) => response.data)
+        .then((data) => setAllQuestions(data.results));
+    }
+  }
+
+  useEffect(getData, [options]);
+
+  if (questions) {
+    console.log(questions[0]);
+  }
 
   return (
     <div className="questionsContainer">
